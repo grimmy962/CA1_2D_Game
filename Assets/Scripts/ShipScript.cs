2d75c7d68eb8128c4898bd296619f6e5f2e74444
grimmy962 <145358834+grimmy962@users.noreply.github.com>
2023-10-15 02:21:34 +0100

    15.10. - adding horizontal, vertical movement

--- Assets/Scripts/ShipScript.cs
+++ Assets/Scripts/ShipScript.cs
@@ -6,6 +6,8 @@ public class ShipScript : MonoBehaviour
 {
     //it's public because we want it to show up in unity, so that we can change up the value while we're play testing the game
     public float moveSpeed = 5;
+    public float hInput;
+    public float vInput;
 
     // Start is called before the first frame update
     void Start()
@@ -16,8 +18,15 @@ public class ShipScript : MonoBehaviour
     // Update is called once per frame (about 60 times per second)
     void Update()
     {
+        //getaxisraw = take the button we push and if it's left = -1, right = 1, nothing = 0
+        //"horizontal" - takes this value from the input manager
+        hInput = Input.GetAxisRaw("Horizontal");
+        vInput = Input.GetAxisRaw("Vertical");
+
         //translate = slide around, Vector2 = has x,y
-        //it's movigng too fast, so we need to multiply it by deltTime = will move according to time (1 unit to the right/second)
-        transform.Translate(Vector2.right *moveSpeed* Time.deltaTime);
+        //it's moving too fast, so we need to multiply it by deltTime = will move according to time (1 unit to the right/second)
+        //after adding hInput - if left goes to left, etc.
+        transform.Translate(Vector2.right *hInput *moveSpeed* Time.deltaTime);
+        transform.Translate(Vector2.up *vInput * moveSpeed * Time.deltaTime);
     }
 }
